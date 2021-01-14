f72d18fbfc4ebcb34c16dbe1238d190729aad891
Miral3 <59829606+Miral3@users.noreply.github.com>
2021-01-14 18:15:12 +0900

    Update and rename makeWierdStr to makeWierdStr.js

--- Programmers/makeWierdStr.js
+++ Programmers/makeWierdStr.js
@@ -28,3 +28,10 @@ function solution(s) {
     }
     return answer;
 }
+/*
+function solution(s) {
+    return s.split(' ').map((w) => (
+        w.split('').map((v, i) => 
+        (i%2 ? v.toLowerCase() : v.toUpperCase())).join(''))).join(' ');
+}
+*/
