5bc12db992c98e2755d33e2baade661dad15f14b
Miral3 <59829606+Miral3@users.noreply.github.com>
2021-01-21 16:58:23 +0900

    Update makeMin.js

--- Programmers/makeMin.js
+++ Programmers/makeMin.js
@@ -1,13 +1,22 @@
-function solution(A,B){
-    var answer = [];
-    const len = A.length;
+// function solution(A,B){
+//     var answer = [];
+//     const len = A.length;
     
-    for(let i = 0; i < len; i++) {
-        let min = Math.min.apply(null, A);
-        let max = Math.max.apply(null, B);
-        answer[i] = min*max;
-        A.splice(A.indexOf(min),1);
-        B.splice(B.indexOf(max),1);
-    }
-    return answer.reduce((a,b)=>a+b);
+//     for(let i = 0; i < len; i++) {
+//         let min = Math.min.apply(null, A);
+//         let max = Math.max.apply(null, B);
+//         answer[i] = min*max;
+//         A.splice(A.indexOf(min),1);
+//         B.splice(B.indexOf(max),1);
+//     }
+//     return answer.reduce((a,b)=>a+b);
+// }
+
+function solution(A,B){
+    A.sort((x,y) => x-y);
+    B.sort((x,y) => y-x);
+    
+    return A.reduce((a,c,i) => {
+        return a+(c * B[i]) ;
+    }, 0);
 }
